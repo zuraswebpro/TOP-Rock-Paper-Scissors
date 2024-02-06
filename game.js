549f6dfeda9d19043fbf113028a4651d0a4892ed
Nick Z <46430670+zuraswebpro@users.noreply.github.com>
2024-02-06 15:24:47 -0800

    created playGame function added toLowerCase method for string conversion

--- game.js
+++ game.js
@@ -5,8 +5,27 @@ function getComputerChoice() {
   return compChoice[computerChoice];
 }
 
-console.log(getComputerChoice());
-
 function playRound(playerSelection, computerSelection) {
-      
+  if (playerSelection == computerSelection) {
+    return "It's a tie! Play again!";
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "Player Wins!";
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "Player Wins!";
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "Player Wins!";
+  } else {
+    return "Computer Wins!";
+  }
 }
+const playerSelection = prompt("What is your choice?").toLowerCase();
+const computerSelection = getComputerChoice().toLowerCase();
+
+function playGame() {
+  return playRound(playerSelection, computerSelection);
+}
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
+console.log(playGame());
